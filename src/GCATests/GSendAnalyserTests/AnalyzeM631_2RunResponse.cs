e3d42a608a7872d77c50498cf7b503281a1282fa
Si Carter <s1cart3r@gmail.com>
2023-07-25 17:21:48 +0200

    Add non functional unit tests to solution

---
+++ src/GCATests/GSendAnalyserTests/AnalyzeM631_2RunResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GSendTests.GSendAnalyserTests
+{
+    [ExcludeFromCodeCoverage]
+    [TestClass]
+    public class AnalyzeM631_2RunResponse
+    {
+        [TestMethod]
+        public void doit()
+        {
+            Assert.IsFalse(true);
+        }
+    }
+}
