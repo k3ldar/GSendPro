b6b6ca626e137dfa1e37cde8e8cd2e1cb492e4e3
Si Carter <s1cart3r@gmail.com>
2024-01-12 06:00:45 +0100

    unit tests

---
+++ src/GCATests/Plugins/HeartbeatTests/HeartbeatsPluginTests.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+using GSendControls.Abstractions;
+using GSendControls.Plugins.InternalPlugins.Hearbeats;
+
+using GSendShared.Plugins;
+
+using GSendTests.Mocks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GSendTests.Plugins.HeartbeatTests
+{
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public sealed class HeartbeatsPluginTests
+    {
+        [TestMethod]
+        public void Construct_ValidInstance_Success()
+        {
+            HearbeatsPlugin sut = new();
+            Assert.IsNotNull(sut);
+            Assert.IsInstanceOfType<IGSendPluginModule>(sut);
+            Assert.AreEqual("Heartbeats", sut.Name);
+            Assert.AreEqual(1u, sut.Version);
+            Assert.AreEqual(PluginHosts.Sender, sut.Host);
+            Assert.AreEqual(PluginOptions.HasControls, sut.Options);
+            Assert.IsNull(sut.MenuItems);
+            Assert.IsNull(sut.ToolbarItems);
+            Assert.IsFalse(sut.ReceiveClientMessages);
+        }
+
+        [TestMethod]
+        public void Validate_ControlItems_Success()
+        {
+            IPluginMenu parentMenu = new MockPluginMenu("test", MenuType.MenuItem, null);
+            MockSenderPluginHost pluginHost = new(parentMenu);
+
+            HearbeatsPlugin sut = new();
+            sut.Initialize(pluginHost);
+            IReadOnlyList<IPluginControl> controlItems = sut.ControlItems;
+
+            Assert.AreEqual(1, controlItems.Count);
+            Assert.AreEqual("Heartbeat Graphs", controlItems[0].Text);
+        }
+
+        [TestMethod]
+        public void ClientMessageReceived_DoesNotThrowException()
+        {
+            HearbeatsPlugin sut = new();
+            bool exceptionRaised = false;
+            try
+            {
+                sut.ClientMessageReceived(null);
+            }
+            catch
+            {
+                exceptionRaised = true;
+            }
+
+            Assert.IsFalse(exceptionRaised);
+        }
+
+        [TestMethod]
+        public void Initialize_DoesNotThrowException()
+        {
+            HearbeatsPlugin sut = new();
+            bool exceptionRaised = false;
+            try
+            {
+                sut.Initialize(null);
+            }
+            catch
+            {
+                exceptionRaised = true;
+            }
+
+            Assert.IsFalse(exceptionRaised);
+        }
+    }
+}
